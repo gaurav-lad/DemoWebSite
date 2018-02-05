3f14b5a7d6083b730c99fb86c244315d8ed91e4d
gaurav-lad <gaurav.lad02@gmail.com>
2018-02-06 01:33:09 +0530

    slide 38

--- DemoWebSite/DemoWebSite/Scripts/Script.js
+++ DemoWebSite/DemoWebSite/Scripts/Script.js
@@ -2,47 +2,62 @@
 
 var app = angular.module("Demo", ["ngRoute"])
     .config(function ($routeProvider, $locationProvider) {
+        $routeProvider.caseInsensitiveMatch = true;
         $routeProvider
             .when("/home", {
             templateUrl: "Templates/home.html",
-            controller: "homeController"
+            controller: "homeController",
+            controllerAs: "homeCtrl",
             })
             .when("/courses", {
                 templateUrl: "Templates/courses.html",
-                controller: "coursesController"
+                controller: "coursesController",
+                controllerAs: "coursesCtrl"
             })
             .when("/students", {
                 templateUrl: "Templates/students.html",
-                controller: "studentsController"
+                controller: "studentsController",
+                controllerAs: "studentsCtrl"
             })
             .when("/students/:id", {
                 templateUrl: "Templates/studentsDetails.html",
-                controller: "studentDetailsController"
+                controller: "studentDetailsController",
+                controllerAs: "studentDetailsCtrl"
             })
             .otherwise({
                 redirectTo:"/home"
             })
         $locationProvider.html5Mode(true);
     })
-    .controller("homeController", function ($scope) {
-        $scope.message = "Home Page";
+    .controller("homeController", function () {
+        this.message = "Home Page";
     })
-    .controller("coursesController", function ($scope) {
-        $scope.courses = ["C#", "JAVA", "Angular", "MySQL"];
+    .controller("coursesController", function () {
+        this.courses = ["C#", "JAVA", "Angular", "MySQL"];
     })
-    .controller("studentsController", function ($scope, $http) {
+    .controller("studentsController", function ($http, $route, $scope) {
+        $scope.$on("$locationChangeStart", function (event, next, current) {
+            if (!confirm("Are you sure you want to navigate away from this page to "+next)) {
+                event.preventDefault();
+            }
+        })
+        var vm = this;
+        vm.reloadData = function () {
+            $route.reload();
+        }
         $http.get("StudentService.asmx/GetAllStudents")
             .then(function (response) {
-                $scope.students = response.data;
+                vm.students = response.data;
             })
     })
-    .controller("studentDetailsController", function ($scope, $http, $routeParams) {
+    .controller("studentDetailsController", function ($http, $routeParams) {
+        var vm = this;
         $http({
             url:"StudentService.asmx/GetStudent",
             params:{id:$routeParams.id},
             method:"get"
         })
         .then(function (response) {
-            $scope.student = response.data;
+            vm.student = response.data;
         })
     })
